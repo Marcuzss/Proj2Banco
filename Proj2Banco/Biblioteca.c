6ffd5aa5ed3a7ce19c0ee994294ac41675cd1f64
joaorogante <143849096+joaorogante@users.noreply.github.com>
2023-10-17 19:35:06 -0300

    Update Biblioteca.c

--- Proj2Banco/Biblioteca.c
+++ Proj2Banco/Biblioteca.c
@@ -1,1 +1,10 @@
-
+void listarClientes(Cliente *clientes, int numClientes) {
+    int i;
+    for (i = 0; i < numClientes; i++) {
+        printf("Nome: %s\n", clientes[i].nome);
+        printf("CPF: %s\n", clientes[i].cpf);
+        printf("Tipo de conta: %s\n", clientes[i].tipoConta);
+        printf("Saldo: %.2lf\n", clientes[i].saldo);
+        printf("---------------\n");
+    }
+}
