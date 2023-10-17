018f9503f510b5b2c21b9102e7030ec28baf35dc
joaorogante <143849096+joaorogante@users.noreply.github.com>
2023-10-17 19:37:14 -0300

    Update Biblioteca.c

--- Proj2Banco/Biblioteca.c
+++ Proj2Banco/Biblioteca.c
@@ -1,1 +1,21 @@
+void deposito(Cliente *clientes, int numClientes) {
+    char cpf[12];
+    double valor;
+
+    printf("Digite o CPF do cliente: ");
+    scanf(" %[^\n]", cpf);
+    printf("Digite o valor: ");
+    scanf("%lf", &valor);
+
+    int i;
+    for (i = 0; i < numClientes; i++) {
+        if (strcmp(clientes[i].cpf, cpf) == 0) {
+            clientes[i].saldo += valor;
+            printf("Depósito realizado com sucesso.\n");
+            return;
+        }
+    }
+
+    printf("CPF não encontrado.\n");
+}
 
