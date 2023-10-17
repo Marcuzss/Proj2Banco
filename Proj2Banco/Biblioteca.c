7dd175e4f5963e75c1d7a9b8f65ebb212c577eab
joaorogante <143849096+joaorogante@users.noreply.github.com>
2023-10-17 19:37:56 -0300

    Update Biblioteca.c

--- Proj2Banco/Biblioteca.c
+++ Proj2Banco/Biblioteca.c
@@ -1,1 +1,32 @@
+void extrato(Cliente *clientes, int numClientes) {
+    char cpf[12], senha[20];
 
+    printf("Digite o CPF do cliente: ");
+    scanf(" %[^\n]", cpf);
+    printf("Digite a senha: ");
+    scanf(" %[^\n]", senha);
+
+    int i;
+    for (i = 0; i < numClientes; i++) {
+        if (strcmp(clientes[i].cpf, cpf) == 0 && strcmp(clientes[i].senha, senha) == 0) {
+             char nomeArquivo[100];
+            printf("Digite o nome do arquivo para salvar o extrato: ");
+            scanf(" %[^\n]", nomeArquivo);
+
+            FILE *arquivo = fopen(nomeArquivo, "w");
+            if (arquivo == NULL) {
+                printf("Erro ao abrir o arquivo.\n");
+                return;
+            }
+
+            fprintf(arquivo, "Extrato do cliente: %s\n", clientes[i].nome);
+            fprintf(arquivo, "--------------------\n");
+
+            fclose(arquivo);
+            printf("Extrato salvo com sucesso.\n");
+            return;
+        }
+    }
+
+    printf("CPF ou senha incorretos.\n");
+}
