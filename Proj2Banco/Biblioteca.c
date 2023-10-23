c7cff7519b10b83c8aae05a25a5edcedc6ef1314
Marcuzss <146141363+Marcuzss@users.noreply.github.com>
2023-10-23 19:24:44 -0300

    Update Biblioteca.c
    Mudança no codigo para suportar a função extrato

--- Proj2Banco/Biblioteca.c
+++ Proj2Banco/Biblioteca.c
@@ -11,6 +11,12 @@ void deposito(Cliente *clientes, int numClientes) {
     for (i = 0; i < numClientes; i++) {
         if (strcmp(clientes[i].cpf, cpf) == 0) {
             clientes[i].saldo += valor;
+            
+            strcpy(clientes[i].operacoes[clientes[i].numOperacoes].tipo, "Depósito");
+            clientes[i].operacoes[clientes[i].numOperacoes].valor = valor;
+            clientes[i].operacoes[clientes[i].numOperacoes].tarifa = 0.0;
+            clientes[i].operacoes[clientes[i].numOperacoes].saldoApos = clientes[i].saldo;
+            clientes[i].numOperacoes++;
             printf("Depósito realizado com sucesso.\n");
             return;
         }
